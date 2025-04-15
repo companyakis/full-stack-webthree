e0e00a31cecb083228704ecd153429f807a70e91
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-15 19:15:22 +0300

    Create part-y-0004-basic-types2.rs

---
+++ part-y-0004-basic-types2.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    // tuple
+    
+    let income_and_age: (u16, u8) = (5500, 34);
+    println!("Age: {}", income_and_age.1);
+    println!("Income: {}", income_and_age.0);
+    
+    // str -> text with a length known at runtime
+    
+    let my_name = "Mustafa";
+    
+    // char
+    
+    let first_letter = 'a';
+}
