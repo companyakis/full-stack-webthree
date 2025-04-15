355abc20b9dd737ead35f0a1364c634e99e88eb2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-15 19:10:46 +0300

    Create part-y-0004-basic-types1.rs

---
+++ part-y-0004-basic-types1.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    // unsigned integers -> from u8 to u128 
+    let age: u8 = 17;
+    let yearly_sales_usd: u128 = 5_654_747;
+    let year = 2025u16;
+    
+    
+    //signed integers -> from i8 to i128
+    let diff: i8 = 3 - 101;
+    let budget = 465_788i64;
+    
+    // floats -> f32, f64
+    let pi1: f32 = 22.0 / 7.0;
+    let pi2: f64 = 22.0 / 7.0;
+    
+    // bools
+    let is_completed: bool = false;
+    let is_ready = true;
+
+}
