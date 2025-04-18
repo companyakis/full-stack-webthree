23348b8379720a911042437ae9cd4c7ea82d94ba
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-18 13:04:51 +0300

    Create part-y-0006-constant.rs

---
+++ part-y-0006-constant.rs
@@ -0,0 +1,21 @@
+const PI_1: f32 = 22.0 / 7.0;
+const PI_2: f64 = 22.0 / 7.0;
+
+const FOUNDER: &str = "Mustafa Büyükdereli";
+
+const STARTUP_YEAR: u16 = 2025;
+
+
+fn main() {
+
+    println!("{PI_1}");
+    println!("{PI_2}");
+    println!("{FOUNDER}");
+    println!("{STARTUP_YEAR}")
+
+}
+
+// 3.142857
+// 3.142857142857143
+// Mustafa Büyükdereli
+// 2025
