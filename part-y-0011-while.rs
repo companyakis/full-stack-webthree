a014ab67e3e6ed79bf8e4da1cd3741245e27c304
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-21 11:20:21 +0300

    Create part-y-0011-while.rs

---
+++ part-y-0011-while.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let mut counter: u8 = 0;
+    
+    while counter < 20 {
+
+        counter =  2 * counter + 1;
+    }
+  
+    println!("Counter final value: {counter}");
+  
+}
+
+// Counter final value: 31
